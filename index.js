5426634481e2d50b7fd1da7186073db2b6f593d4
Maria <maripapa14@gmail.com>
2021-02-08 01:09:38 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const twit = require('twit');
+
+const T = new twit({
+    consumer_key:         'EyAE2doCANBuvNg3ONdAlKD3I',
+    consumer_secret:      'p8eVeyaKam1ZaLvlRCqZ1lytEqhZWFVagSnzpdkyHugZQ7r8V8',
+    access_token:         '1350162848236920843-tCXCewbbGCHWptw6THYvXXvOnGnGId',
+    access_token_secret:  'Vfs5Q2RT7R588pkBPdI2pnDl4zgcVHityvA0sQZmXWJhu',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.get('/tweets/:search',function(req,res){
+    T.get('search/tweets', { q:req.params.search, count: 5 }, function(err, data, response) {
+        res.json(data);
+    })
+})
+
+app.post('/comment/',function(req,res){
+    console.log(req.body.comment);
+    T.post('statuses/update', {status: req.body.comment}, function(err, data, response) {
+        res.json(data)
+    })
+})
+
+app.listen(3000)
